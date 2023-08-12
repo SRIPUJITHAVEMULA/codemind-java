5fd9f180dbddace9ef76fe8344beba361afbeab5
SRIPUJITHAVEMULA <119916263+SRIPUJITHAVEMULA@users.noreply.github.com>
2023-08-12 09:27:10 +0530

    import java.util.Scanner; public class nums{     public static void main(String Args[])     {         Scanner sc=new Scanner(System.in);         float a,b,c;         a=sc.nextFloat();         b=sc.nextFloat();         c=a*b;         System.out.printf("%.2f",c);     } }

---
+++ Program_to_Multiply_Two_Floating-Point_Numbers.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class nums{
+    public static void main(String Args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        float a,b,c;
+        a=sc.nextFloat();
+        b=sc.nextFloat();
+        c=a*b;
+        System.out.printf("%.2f",c);
+    }
+}
