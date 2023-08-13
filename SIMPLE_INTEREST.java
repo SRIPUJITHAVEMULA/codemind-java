db14a98b715cdd32b0654f99935ec0e8def0c1f5
SRIPUJITHAVEMULA <119916263+SRIPUJITHAVEMULA@users.noreply.github.com>
2023-08-13 20:49:39 +0530

    import java.util.Scanner; public class simple{     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int p,t,r;         p=sc.nextInt();         t=sc.nextInt();         r=sc.nextInt();         int s;         s=(p*t*r)/100;         System.out.println(s);     } }

---
+++ SIMPLE_INTEREST.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class simple{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int p,t,r;
+        p=sc.nextInt();
+        t=sc.nextInt();
+        r=sc.nextInt();
+        int s;
+        s=(p*t*r)/100;
+        System.out.println(s);
+    }
+}
