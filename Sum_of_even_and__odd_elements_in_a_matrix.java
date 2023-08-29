b9d6a05981da8ba45acfab52a0729511eb4cf39c
SRIPUJITHAVEMULA <119916263+SRIPUJITHAVEMULA@users.noreply.github.com>
2023-08-29 11:44:48 +0530

    import java.util.Scanner; public class Program{     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int m,n,se=0,so=0;         m=sc.nextInt();         n=sc.nextInt();         int arr[][]=new int[m][n];         for(int i=0;i<m;i++)         {             for(int j=0;j<n;j++)             {                 arr[i][j]=sc.nextInt();             }         }         for(int i=0;i<m;i++)         {             for(int j=0;j<n;j++)             {                 if(arr[i][j]%2==0)                 {                    se=se+arr[i][j];                 }                 else                 {                     so=so+arr[i][j];                 }             }         }         System.out.print(se+" ");         System.out.print(so);     } }

---
+++ Sum_of_even_and__odd_elements_in_a_matrix.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class Program{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int m,n,se=0,so=0;
+        m=sc.nextInt();
+        n=sc.nextInt();
+        int arr[][]=new int[m][n];
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                arr[i][j]=sc.nextInt();
+            }
+        }
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(arr[i][j]%2==0)
+                {
+                   se=se+arr[i][j]; 
+                }
+                else
+                {
+                    so=so+arr[i][j];
+                }
+            }
+        }
+        System.out.print(se+" ");
+        System.out.print(so);
+    }
+}
