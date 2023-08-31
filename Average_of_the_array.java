429e17b18960cc314dcc90cab60b2a4343e7a1fb
SRIPUJITHAVEMULA <119916263+SRIPUJITHAVEMULA@users.noreply.github.com>
2023-08-31 22:28:17 +0530

    import java.util.Scanner; public class Program{     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int arr[]=new int[n];         for(int i=0;i<n;i++)         {             arr[i]=sc.nextInt();         }         int sum=0;         for(int i=0;i<n;i++)         {             sum+=arr[i];         }         float avg;         avg=(float)sum/n;         System.out.printf("%.2f",avg);     } }

---
+++ Average_of_the_array.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Program{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        int sum=0;
+        for(int i=0;i<n;i++)
+        {
+            sum+=arr[i];
+        }
+        float avg;
+        avg=(float)sum/n;
+        System.out.printf("%.2f",avg);
+    }
+}
